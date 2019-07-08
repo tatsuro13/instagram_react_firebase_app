fe8d30e1ef7b1fbf9833592760a9031fb9b4cccc
ad_osuka_t <osuka_t@a-drop.com>
2019-07-08 18:07:58 +0900

    ヘッダーのログインアウトの分岐表示途中

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,11 @@ import { withStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
 import Avatar from "@material-ui/core/Avatar";
 import Typography from "@material-ui/core/Typography";
+import firebase from "firebase/app";
+import "firebase/auth";
 
 const styles = theme => ({
   root: {
@@ -16,10 +19,91 @@ const styles = theme => ({
   },
   button: {
     margin: theme.spacing.unit
+  },
+  rightIcon: {
+    marginLeft: theme.spacing.unit
+  },
+  avatar: {
+    margin: 10,
+    backgroundColor: "white"
   }
 });
 
 class Header extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isLogin: false,
+      username: "",
+      profilePicUrl: ""
+    };
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({
+          isLogin: true,
+          username: user.displayName,
+          profilePicUrl: user.photoURL
+        });
+      } else {
+        this.setState({
+          isLogin: false,
+          username: "",
+          profilePicUrl: ""
+        });
+      }
+    });
+  }
+
+  googleLogin = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithRedirect(provider);
+  };
+
+  googleSignOut = () => {
+    firebase.auth().signOut();
+  };
+
+  renderLoginComponent = classes => {
+    return (
+      <Button
+        color="inherit"
+        className={classes.button}
+        onClick={this.googleLogin}
+      >
+        Login with Google
+      </Button>
+    );
+  };
+
+  renderLoginedComponent = classes => {
+    return (
+      <div>
+        <Button
+          color="inherit"
+          className={classes.button}
+          onClick={this.isLogin}
+        >
+          <Avatar
+            alt="profile image"
+            src={`${this.state.profilePicUrl}`}
+            className={classes.avatar}
+          />
+          {this.state.username}
+        </Button>
+        <Button
+          color="inherit"
+          className={classes.button}
+          onClick={this.googleSignOut}
+        >
+          Sign Out
+        </Button>
+      </div>
+    );
+  };
   render() {
     const { classes } = this.props;
     return (
@@ -33,13 +117,9 @@ class Header extends Component {
             >
               Firebase Videos
             </Typography>
-            <Button
-              color="inherit"
-              className={classes.button}
-              onclick={this.googleLogin}
-            >
-              Login with Google
-            </Button>
+            {this.state.isLogin
+              ? this.renderLoginedComponent(classes)
+              : this.renderLoginComponent(classes)}
           </Toolbar>
         </AppBar>
       </div>
