ef6c6aa236349f241ca93ce6eed83ec076bce6fd
tatsuro13 <oscarwild13@gmail.com>
2019-07-21 12:52:54 +0900

    動画アップロード機構fireStorage実装

--- src/components/VideoUpload.js
+++ src/components/VideoUpload.js
@@ -1,15 +1,43 @@
 import React, {Component} from 'react'
+import firebase from 'firebase/app'
+import 'firebase/storage'
 
 class Upload extends Component{
+    constructor(props){
+        super(props);
+        this.state = {video: null}
+    }
+
     handleChange = event => {
-        return;
+        event.preventDefault();
+        const video = event.target.files[0];
+
+        this.setState({video});
     }
+
     handleSubmit = event => {
-        return;
+        event.preventDefault();
+        this.fileUpload(this.state.video);
     }
+
+    async fileUpload(video) {
+        try{
+            const userUid = firebase.auth().currentUser.uid;
+            const filePath = `videos/${userUid}/${video.name}`;
+            const videoStorageRef = firebase.storage().ref(filePath);
+            const fileSnaopshot = await
+            videoStorageRef.put(video);
+
+            console.log(fileSnaopshot);
+        }catch(error){
+            console.log(error);
+            return;
+        }
+    }
+
     render(){
         return(
-            <form onsubmit={e => this.handleSubmit(e)}>
+            <form onSubmit={e => this.handleSubmit(e)}>
                 <h2>Video Uplolad</h2>
                 <input type="file" accept="video/*" onChange={e => this.handleChange(e)} />
                 <button type="submit">
