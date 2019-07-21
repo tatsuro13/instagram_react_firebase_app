389f5208492cd9514221b5af58761ccd1f343d01
tatsuro13 <oscarwild13@gmail.com>
2019-07-21 19:36:55 +0900

    mp4をfirestoreに保存

--- src/components/VideoUpload.js
+++ src/components/VideoUpload.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import firebase from 'firebase/app'
 import 'firebase/storage'
+import 'firebase/firestore';
+import _ from 'lodash'
 
 class Upload extends Component{
     constructor(props){
@@ -25,14 +27,45 @@ class Upload extends Component{
             const userUid = firebase.auth().currentUser.uid;
             const filePath = `videos/${userUid}/${video.name}`;
             const videoStorageRef = firebase.storage().ref(filePath);
+            const idToken = await
+            firebase.auth().currentUser.getIdToken(true);
+            const metadataForStorage = {
+                customMetadata: {
+                    idToken: idToken
+                }
+            };
             const fileSnaopshot = await
-            videoStorageRef.put(video);
+            videoStorageRef.put(video, metadataForStorage);
 
-            console.log(fileSnaopshot);
+            // mp4以外の動画は、cloudFunctions上で、トランスコードした後にメタデータをFirestoreに保存する
+            if(video.type === 'video/mp4'){
+                const downloadURL = await
+                videoStorageRef.getDownloadURL();
+                let metadataForFirestore = _.omitBy(fileSnaopshot.metadata, _.isEmpty);
+                metadataForFirestore = Object.assign(metadataForFirestore, {downloadURL: downloadURL});
+
+                this.saveVideoMetadata(metadataForFirestore)
+            }
+
+            if(fileSnaopshot.state === 'success'){
+                console.log(fileSnaopshot);
+                this.setState({video: null});
+            }else{
+                console.log(fileSnaopshot);
+                alert('ファイルのアップロードに失敗しました')
+            }
         }catch(error){
             console.log(error);
             return;
         }
+    }
+
+    async saveVideoMetadata(metadata){
+        const userUid = firebase.auth().currentUser.uid;
+        const videoRef = firebase.firestore().doc(`users/${userUid}`).collection('videos').doc();
+        metadata = Object.assign(metadata, {uid: videoRef.id})
+        
+        await videoRef.set(metadata, {merge: true});
     }
 
     render(){
